61a9a920dcb8f12a15bd4c08ee0482dac016f0df
skatesham <sham.vinicius@gmail.com>
2018-12-19 14:31:23 -0200

    aula 4 - listar Produtos

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
@@ -25,10 +28,18 @@ public class ProdutosController {
 		
 	}
 	
-	@RequestMapping("/produtos")
+	@RequestMapping(name="/produtos", method=RequestMethod.POST)
 	public String gravar(Produto produto){
 		System.out.println(produto);
 		dao.gravar(produto);
 		return "/produtos/ok";
+	}
+	
+	@RequestMapping(name="/produtos", method=RequestMethod.GET)
+	public ModelAndView listar(){
+	    List<Produto> produtos = dao.listar();
+	    ModelAndView modelAndView = new ModelAndView("/produtos/lista");
+	    modelAndView.addObject("produtos", produtos);
+	    return modelAndView;
 	}
 }
